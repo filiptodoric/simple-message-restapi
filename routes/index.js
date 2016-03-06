9724276fa059746bc4cc55e5ed2162a21748e112
Filip Todoric <filiptodoric@gmail.com>
2016-03-06 17:04:32 -0500

    Able to add/delete/get messages. Also checks to see if a message is a palindrome when being added to the database!

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,15 @@ var Message = require('../models/message');
 
 // isPalindrome to be moved somewhere else
 var isPalindrome = function(content)  {
-    return content == content.trim().split('').reverse().join('');
+    content = content.trim().toLowerCase();
+    return content == content.split('').reverse().join('');
 }
 
 
 
 /* Middleware, all requests will come through here first. */
 router.use(function(req, res, next) {
-    console.log("Request received.");
+    console.log("Received a " + req.method + " request.");
     next();
 });
 
@@ -20,22 +21,52 @@ router.get('/', function(req, res) {
   res.json({message: 'Connected to the API'});
 });
 
-router.route('/messages')
-    .post(function(req, res)    {
+/* Adds a new message to the database */
+router.route('/messages').post(function(req, res)    {
         var message = new Message();
         message.title = req.body.title;
         message.content = req.body.content;
         message.isPalindrome = isPalindrome(req.body.content);
 
         message.save(function(err)  {
-            if(err) {
+            if(err)
                 res.send(err);
-            }
-            else    {
+            else
                 res.json({message: 'New message has been created.'});
-            }
-        })
 
+        });
     });
+
+/* Gets all the messages from the database */
+router.route('/messages').get(function(req,res)  {
+        Message.find(function(err, messages)    {
+            if(err)
+                res.send(err);
+            else
+                res.json(messages);
+        })
+    });
+
+/* Gets a specific message */
+router.route('/messages/:message_id').get(function(req, res) {
+    Message.findById(req.params.message_id, function(err, message)  {
+        if(err)
+            res.send(err);
+        else
+            res.json(message);
+    })
+});
+
+/* Delete the message with this id */
+router.route('/messages/:message_id').delete(function(req, res) {
+    Message.remove({ _id: req.params.message_id },
+        function(err, message)  {
+            if(err)
+                res.send(err);
+            else
+                res.json({ message: "The message has been deleted."});
+        }
+    )
+});
 
 module.exports = router;
